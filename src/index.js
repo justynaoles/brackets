267065d5e7ee78249fd41e2ebf4e259144181d3d
Justyna Oleś <justynaoles88@gmail.com>
2020-03-21 18:31:39 +0100

    added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  if(str.length % 2 !== 0) {
+    return false;
+  };
+  const string = str.split('');
+  const arr = [];
+  const openBrackets = new Set(bracketsConfig.map(elConfig => elConfig[0]));
+  const closeBrackets = new Map(bracketsConfig.map(elConfig => [elConfig[1],elConfig[0]]));
+
+    for (let i =0; i<string.length; i++){
+
+    if (openBrackets.has(string[i])) {
+
+      if (string[i] === arr[arr.length-1] && openBrackets.has(string[i]) && closeBrackets.get(string[i])) {
+        arr.pop()
+      } else { 
+        arr.push(string[i])
+      }
+    } else {
+      let lastArrItem = arr.pop();
+      if (lastArrItem !== closeBrackets.get(string[i])){ 
+        return false; 
+      }
+    }
+  }
+ 
+  const result = arr.length === 0;
+  return result;
 }
